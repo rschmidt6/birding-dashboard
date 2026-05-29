93df1b819780a717ed3165f1d84be4af2709d1ef
Ryan Schmidt <rdschmidt88@gmail.com>
2026-05-29 11:17:59 -0500

    added endpoint

--- src/app/services/bird-data.service.ts
+++ src/app/services/bird-data.service.ts
@@ -10,6 +10,7 @@ export class BirdDataService {
   private apiKey = '52hmur36h0mr';
   private baseUrl = 'https://api.ebird.org/v2';
   private regionCode = 'US-TX-453';
+  private travisCountyLatLong = [30.2672, -97.7431];
 
   constructor(private http: HttpClient) {}
 
@@ -40,6 +41,15 @@ export class BirdDataService {
   getRecentNotableObservations() {
     return this.http.get<Bird[]>(
       `${this.baseUrl}/data/obs/${this.regionCode}/recent/notable`,
+      {
+        headers: { 'X-eBirdApiToken': this.apiKey },
+      },
+    );
+  }
+
+  getRecentNearbySpeciesObservations(speciesCode: string) {
+    return this.http.get(
+      `${this.baseUrl}/data/obs/geo/recent/${speciesCode}?lat=${this.travisCountyLatLong[0]}&lng=${this.travisCountyLatLong[1]}`,
       {
         headers: { 'X-eBirdApiToken': this.apiKey },
       },
